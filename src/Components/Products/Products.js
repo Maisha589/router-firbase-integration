699fc7693ae05c08f846f4ac955d0e025729b525
Maisha589 <maisharahman589@gmail.com>
2022-04-25 04:54:47 +0600

    final

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,16 +3,18 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
 
-const auth = getAuth(app);
+const auth = getAuth(app)
 
-const Home = () => {
+const Products = () => {
     const [user] = useAuthState(auth);
     return (
         <div>
-            <h2>This is home</h2>
-            <p>Curent user is: {user ? user.displayName : "Nobody"}</p>
+            <div>
+                <h2>Knock Knock ! who is there?</h2>
+                <h5>{user ? user.displayName : "voooot"}</h5>
+            </div>
         </div>
     );
 };
 
-export default Home;
+export default Products;
